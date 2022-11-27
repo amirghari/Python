c0c6be4649723044fea074dea4a920e3bc170d93
amirghari <99399497+amirghari@users.noreply.github.com>
2022-11-27 19:44:29 +0200

    Done

--- Exercise 13.py
+++ Exercise 13.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 @app.route("/prime_number/<num>")
 def prime_number(num):
     def primenumber(num):
-        for i in range(2, int(math.sqrt(abs(num)))):
-            if num % i == 0:
-                return False
-            else:
-                return True
+        if num == 2 or num == 3:
+            return True
+        else:
+            for i in range(2, num):
+                if num % i == 0:
+                    return False
+                    break
+                elif i == (num-1):
+                    return True
+                    break
 
     response = {
         "Number": num,
@@ -21,5 +26,6 @@ if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
 # Part2
+
 
 
